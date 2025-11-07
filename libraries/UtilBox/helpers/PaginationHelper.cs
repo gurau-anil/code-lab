07d77305a8ce9428d2f631cf7446039e07eaf02e
gurau-anil <gurau.anil@gmail.com>
2025-11-07 14:05:34 -0700

    libraries- utilbox-helpers  pagination helper

---
+++ libraries/UtilBox/helpers/PaginationHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilBox.helpers
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+
+    public static class PaginationHelper
+    {
+        public static PagedResult<T> GetPaged<T>(
+            this IQueryable<T> query, int page, int pageSize)
+        {
+            var totalCount = query.Count();
+            var items = query.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedResult<T>
+            {
+                Items = items,
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+    }
+}
