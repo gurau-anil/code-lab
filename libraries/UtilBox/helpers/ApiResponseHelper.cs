f9fffe28f7b6501b3663250bb7f3e47f78a03b0c
gurau-anil <gurau.anil@gmail.com>
2025-11-11 17:22:09 -0700

    API response helper

---
+++ libraries/UtilBox/helpers/ApiResponseHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace UtilBox.helpers
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+        public string? ErrorCode { get; set; }
+
+        public static ApiResponse<T> CreateSuccess(T data, string? message = null)
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                Data = data,
+                Message = message
+            };
+        }
+
+        public static ApiResponse<T> CreateError(string errorCode, string message)
+        {
+            return new ApiResponse<T>
+            {
+                Success = false,
+                ErrorCode = errorCode,
+                Message = message
+            };
+        }
+    }
+
+}
